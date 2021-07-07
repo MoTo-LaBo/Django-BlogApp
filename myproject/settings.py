bd2815a1e4ef36ae2d107f438f03d0319a495789
MoTo <moto.labo.desgin@gmail.com>
2021-07-07 12:37:50 +0900

    update

--- myproject/settings.py
+++ myproject/settings.py
@@ -160,5 +160,5 @@ except ImportError:
 
 if not DEBUG:
     SECRET_KEY = os.environ['SECRET_KEY']
-    AWS_ACCESS_KEY = os.environ['AWS_ACCESS_KEY']
+    AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
     AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
